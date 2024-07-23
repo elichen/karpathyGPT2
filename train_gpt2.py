92a89ca632c3ef6acde30275d6c529450d5f8c4f
elichen <eli.chen@gmail.com>
2024-07-23 22:35:56 +0000

    u

--- train_gpt2.py
+++ train_gpt2.py
@@ -191,7 +191,9 @@ torch.manual_seed(42)
 if torch.cuda.is_available():
     torch.cuda.manual_seed(42)
 
-train_loader = DataLoaderLite(B=16, T=32)
+train_loader = DataLoaderLite(B=4, T=1024)
+
+torch.set_float32_matmul_precision("high")
 
 model = GPT(GPTConfig()).to(device)
 optim = torch.optim.AdamW(model.parameters(), lr = 3e-4)
@@ -200,13 +202,15 @@ for i in range(50):
     x, y = train_loader.next_batch()
     x, y = x.to(device), y.to(device)
     optim.zero_grad()
-    logits, loss = model(x, y)
+    with torch.autocast(device_type=device, dtype=torch.bfloat16):
+        logits, loss = model(x, y)
     loss.backward()
     optim.step()
     torch.cuda.synchronize()
     t1 = time.time()
     dt = (t1 - t0)*1000
-    print(f"step {i}: loss: {loss.item()}, dt: {dt:.2f}ms")
+    tokens_per_sec = (train_loader.B * train_loader.T) / (t1 - t0)
+    print(f"step {i}: loss: {loss.item()}, dt: {dt:.2f}ms, tok/sec: {tokens_per_sec}")
 
 import sys; sys.exit(0)
 
